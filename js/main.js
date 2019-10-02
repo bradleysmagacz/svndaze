e154f7c091ec50ecec00bd63c006de85d3b21cc3
Bradley Smagacz <bsmagacz@truecar.com>
2019-10-02 15:50:36 -0700

    update links and contact

--- js/main.js
+++ js/main.js
@@ -1,6 +1,42 @@
 jQuery(document).ready(function($) {
    
-   'use strict';
+	 'use strict';
+	 
+	/**
+		* Determine the mobile operating system.
+		* This function returns one of 'iOS', 'Android', 'Windows Phone', or 'unknown'.
+		*
+		* @returns {String}
+		*/
+		function getMobileOperatingSystem() {
+			var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+				// Windows Phone must come first because its UA also contains "Android"
+				if (/windows phone/i.test(userAgent)) {
+						return "Windows Phone";
+				}
+
+				if (/android/i.test(userAgent)) {
+						return "Android";
+				}
+
+				// iOS detection from: http://stackoverflow.com/a/9039885/177710
+				if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+						return "iOS";
+				}
+
+				return "unknown";
+		}
+	
+	var os = getMobileOperatingSystem();
+	var igLink = "https://www.instagram.com/svndze.events";
+	if (os === "iOS") {
+		igLink = "instagram://user?username=svndaze.events";
+	}
+	if (os === "Android") {
+		igLink = "intent://instagram.com/_u/svndaze.events/#Intent;package=com.instagram.android;scheme=https;end"
+	}
+
+	$('#igLink').attr("href", igLink);
    
 	
 	//SMOOTH SCROLL
